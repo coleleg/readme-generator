bd1dabc39e936767fef115112d6a3d34d0011b3f
coleleg <coleleg@gmail.com>
2021-12-20 13:56:06 -0500

    added questions and 'node index' will prompt those questions'

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of this project? (Required)'
+    },+
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub Username?'
+    },
+    {
+        type: 'confirm',
+        name: 'confirmEmail',
+        message: 'Would you like to include your email address?',
+        default: true
+    },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Enter your email address:',
+                when: ({ confirmAbout }) => confirmAbout
+            },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for this project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter any installation instructions for this project:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information for this project:'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines for this project:'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Enter testing instruction for this project:'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
